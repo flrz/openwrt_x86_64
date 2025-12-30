098b9d621fe1a77ac299dbee34d7e6216d16d5d9
lieet <59359907+flrz@users.noreply.github.com>
2025-12-30 14:33:45 +0800

    Add OpenClash feed source to feeds.conf.default

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,4 @@
 
 # Add a feed source
 sed -i '1i src-git flrz https://github.com/flrz/openwrt-packages' feeds.conf.default
+sed -i '2i src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
